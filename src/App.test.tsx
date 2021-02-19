e6935dde58ea8eda1ad48eed6ef950ed841e005f
alightsleeper <alightsleeper@gmail.com>
2021-02-19 12:33:51 -0600

    use userEvent click function

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
-import { render, fireEvent, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-
 import { Provider } from 'react-redux'
 import { store } from './app/store'
 import App from './App'
@@ -21,15 +20,15 @@ test('renders discogs search form and returns results', async () => {
   expect(searchInput).toBeInTheDocument()
 
   userEvent.type(searchInput, 'fugazi')
+  userEvent.click(searchButton)
 
-  fireEvent.click(searchButton)
   let result = await screen.findByText('Fugazi')
   expect(result).toBeInTheDocument()
 
   const viewArtistButton = getByText('Fugazi')
   expect(viewArtistButton).toBeInTheDocument()
 
-  fireEvent.click(viewArtistButton)
+  userEvent.click(viewArtistButton)
   result = await screen.findByText('Fugazi is an American punk rock band that formed in Washington, D.C. in 1987. The band consists of guitarists and vocalists Ian MacKaye and Guy Picciotto, bassist Joe Lally and drummer Brendan Canty. They are noted for their DIY ethic, manner of business practice, and contempt towards the music industry. Fugazi have performed numerous worldwide tours, produced six studio albums, a film and a comprehensive live series, gaining the band critical acclaim and success around the world. The band has been on an indefinite break since 2003.')
   expect(result).toBeInTheDocument()
 })
