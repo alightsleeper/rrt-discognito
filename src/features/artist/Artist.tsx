7e7c00ccde24aeaebfbca07ae86d957a4e404348
alightsleeper <alightsleeper@gmail.com>
2021-02-26 14:37:07 -0600

    sort releases by delta of in_wantlist and in_collection, desc

--- src/features/artist/Artist.tsx
+++ src/features/artist/Artist.tsx
@@ -17,9 +17,13 @@ export const Artist = () => {
             .then(data => data.releases.filter((r: ArtistRelease) => (r.type === "master" && r.role==="Main")))
             .then(filtered => filtered.sort((a: ArtistRelease, b: ArtistRelease) => {
                 return (
-                    // sort releases by most in_wantlist, then least in_collection
-                    (b.stats.community.in_wantlist - a.stats.community.in_wantlist) ||
-                    (a.stats.community.in_collection - b.stats.community.in_collection)
+                    // //sort releases by most in_wantlist, then least in_collection
+                    // (b.stats.community.in_wantlist - a.stats.community.in_wantlist) ||
+                    // (a.stats.community.in_collection - b.stats.community.in_collection)
+                    
+                    //sort releases by delta of in_wantlist and in_collection, desc
+                    (b.stats.community.in_wantlist - b.stats.community.in_collection) -
+                    (a.stats.community.in_wantlist - a.stats.community.in_collection)
                 );
             }))
             .then(sorted => setReleases(sorted))
@@ -33,6 +37,7 @@ export const Artist = () => {
                 <h3>{release.title} {release.year && `(${release.year})`}</h3>
                 <p><strong>In Wantlist: </strong>{release.stats.community.in_wantlist}</p>
                 <p><strong>In Collection: </strong>{release.stats.community.in_collection}</p>
+                <p><strong>Delta: </strong>{release.stats.community.in_wantlist - release.stats.community.in_collection}</p>
                 <p><strong>URL: </strong>{release.resource_url}</p>
                 <hr/>
             </div>
@@ -40,7 +45,7 @@ export const Artist = () => {
     });
 
     return (
-        <div className="content">
+        <div className="content">          
             <h1 className="center">{artist.name}</h1>
             <p>{artist.profile}</p>
             <p className="center">{releasesLoading && "Loading..."}</p>
